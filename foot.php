3ea20f272c552ab41b917e716ba367fa4719209c
Aranya7750 <annaranya7750@gmail.com>
2021-11-27 21:00:14 +0700

    foot,index

---
+++ foot.php
@@ -0,0 +1,24 @@
+style>
+    .ft{
+        padding-top:10px;
+        padding-bottom:10px;
+    }
+</style>
+
+<div class="jambotron text-center bg-warning text-white" style="margin-bottom:0;">
+<div class="row">
+
+    <div class="col-sm-6">
+    <span class="ft">
+    Ⓒ งานกลุ่ม 3 คน --- 022 025 026
+    </span>
+    </div>
+
+    <div class="col-sm-6 text-center">
+    <span class ="ft">
+        จัดทำโดย || นันทิพัฒน์ 022 || อรัญญา 025 || พชรพล 026
+    </span >
+</div>
+
+</div>
+</div>
---
+++ index.php
@@ -0,0 +1,11 @@
+<body>
+<?php include_once ('head.php')?>
+<?php include_once ('jum.php')?>
+<?php include_once ('navi.php') ?>
+
+<div class="contianer" stlye="margin-top:30px">
+    <h2> คิดว่าจะทำเป็นหน้า index</h2>
+</div>
+<br><br>
+<?php include_once ('foot.php') ?>
+</body>
